3047680109328f2bc7178b39181f5fa2a50d528d
farshidhesami <farshidhesami@gmail.com>
2024-06-30 13:14:42 +0330

    updated

--- src/Dental_Implant_Sandblasting/config/configuration_manager.py
+++ src/Dental_Implant_Sandblasting/config/configuration_manager.py
@@ -1,14 +1,17 @@
 from Dental_Implant_Sandblasting.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH, SCHEMA_FILE_PATH
 from Dental_Implant_Sandblasting.utils.common import read_yaml, create_directories
-from Dental_Implant_Sandblasting.entity.config_entity import DataIngestionConfig
+from Dental_Implant_Sandblasting.entity.config_entity import (DataIngestionConfig,
+                                                              DataValidationConfig)
 
 
+# Configuration Manager class to read the configuration files and return the configuration objects (data ingestion config)
+# The ConfigurationManager class will have the following methods:
 class ConfigurationManager:
     def __init__(
         self,
-        config_filepath=CONFIG_FILE_PATH,
-        params_filepath=PARAMS_FILE_PATH,
-        schema_filepath=SCHEMA_FILE_PATH):
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH,
+        schema_filepath = SCHEMA_FILE_PATH):
 
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
@@ -16,12 +19,16 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
 
+
+
+
+# get_data_ingestion_config() method to return the data ingestion config object
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
         create_directories([config.root_dir])
         
-        # DataIngestionConfig is being defined as a namedtuple with four fields:
+        # DataIngestionConfig object to be returned by the get_data_ingestion_config() method
         data_ingestion_config = DataIngestionConfig(
             root_dir=config.root_dir,
             source_URL=config.source_URL,
@@ -32,3 +39,18 @@ class ConfigurationManager:
         return data_ingestion_config
 
 
+# get_data_validation_config() method to return the data validation config object
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
--- src/Dental_Implant_Sandblasting/entity/config_entity.py
+++ src/Dental_Implant_Sandblasting/entity/config_entity.py
@@ -1,9 +1,19 @@
+# Config entity for data ingestion process (data ingestion config)
 from dataclasses import dataclass
 from pathlib import Path
 
-@dataclass(frozen=True)
+
+@dataclass(frozen=True)             # This code copy from 01_data_ingestion.py
 class DataIngestionConfig:
     root_dir: Path
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+    
+
+@dataclass(frozen=True)              # This code copy from 02_data_validation.py
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir: Path
+    all_schema: dict
