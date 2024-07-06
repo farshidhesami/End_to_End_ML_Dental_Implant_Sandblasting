91aa6a37b9cee504facfdd046595b7fdcf9ee858
farshidhesami <farshidhesami@gmail.com>
2024-07-06 17:26:25 +0330

    Added 03_data_transformation

--- src/Dental_Implant_Sandblasting/entity/config_entity.py
+++ src/Dental_Implant_Sandblasting/entity/config_entity.py
@@ -2,18 +2,29 @@
 from dataclasses import dataclass
 from pathlib import Path
 
-
-@dataclass(frozen=True)             # This code copy from 01_data_ingestion.py
+@dataclass(frozen=True)             # from 01_data_ingestion.py
 class DataIngestionConfig:
     root_dir: Path
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
 
-
-@dataclass(frozen=True)              # This code copy from 02_data_validation.py
+@dataclass(frozen=True)              # from 02_data_validation.py
 class DataValidationConfig:
     root_dir: Path
     STATUS_FILE: str
     unzip_data_dir: Path
     all_schema: dict
+
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    transformed_train_dir: Path
+    transformed_test_dir: Path
+    test_size: float
+    random_state: int
+    polynomial_features_degree: int
+    scaling_method: str
+
+
